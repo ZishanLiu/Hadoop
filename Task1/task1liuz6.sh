f76eca949c7f0dc736df8f128e64d37c26bd6524
ZishanLiu <Zishan.liu@outlook.com>
2019-05-07 22:19:24 -0400

    lab7

--- Task1/task1liuz6.sh
+++ Task1/task1liuz6.sh
@@ -1,6 +1,6 @@
 sqoop import --connect jdbc:mysql://$1/sqooptest --username hive --table Employees --split-by eid --target-dir /tmp/sqoopOutput -P
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username hive --table Employees -m 1 --split-by eid --target-dir /tmp/sqoopOutput -P
+sqoop import --connect jdbc:mysql://$1/sqooptest --username hive --table Employees -m 1 --split-by eid --target-dir /tmp/sqoopMapOutput -P
 
 sqoop import --connect jdbc:mysql://$1/sqooptest --username hive --table Employees -m 2 --target-dir /tmp/sqoopSeqOutput --as-sequencefile -P
 
@@ -14,4 +14,4 @@ sqoop import --connect jdbc:mysql://$1/sqooptest --username hive -m 2 --table Em
 
 sqoop import --connect jdbc:mysql://$1/sqooptest --username hive -m 2 --table Employees --hive-import --create-hive-table --hive-table sqooptest.Employees --fields-terminated-by '\t' --null-string 'This is a Null String' --null-non-string '\\N' -P
 
-sqoop import-all-tables --connect jdbc:mysql://$1/sqooptest --username hive -m 1 --warehouse-dir /tmp/sqoopAll --fields-terminated-by ',' --null-string '\\N' --null-non-string '\\N' -P
+sqoop import-all-tables --connect jdbc:mysql://$1/sqooptest --username hive -m 1 --warehouse-dir /tmp/sqoopAll/ -P
