914bc7f765e0ec3e3c03d2af489fa071d6a12453
ZishanLiu <Zishan.liu@outlook.com>
2019-05-09 04:47:06 -0400

    exam2

--- Exam2/Task5/task5.sh
+++
@@ -1,1 +0,0 @@
-sqoop export --connect jdbc:mysql://$1/sqooptest --username hive -m 1 --table examData --export-dir /tmp/sqoop/exam/username=liuz6 --input-fields-terminated-by '\t' --input-null-string 'NULL String' --input-null-non-string '\\N' -P
